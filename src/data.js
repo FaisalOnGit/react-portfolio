54ec06a44e2a5ccecb083db22b92d70ce3a8fa9c
FaisalOnGit <faisaloktovian1@gmail.com>
2022-08-22 16:19:13 +0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: 'https://www56.zippyshare.com/scaled/OD9PJHAm/file.html',
+    link: 'https://50projects50days.com/projects/drink-water/',
+  },
+  {
+    id: 2,
+    img: 'https://www74.zippyshare.com/scaled/QX9Voqvl/file.html',
+    link: 'https://50projects50days.com/projects/theme-clock/',
+  },
+  {
+    id: 3,
+    img: 'https://www53.zippyshare.com/scaled/I7cT06Q9/file.html',
+    link: 'https://50projects50days.com/projects/notes-app/',
+  },
+  {
+    id: 4,
+    img: 'https://www77.zippyshare.com/scaled/4k8E6DMp/file.html',
+    link: 'https://50projects50days.com/projects/drawing-app/',
+  },
+  {
+    id: 5,
+    img: 'https://www83.zippyshare.com/scaled/J9l5ZP3u/file.html',
+    link: 'https://50projects50days.com/projects/todo-list/',
+  },
+  {
+    id: 6,
+    img: 'https://www14.zippyshare.com/scaled/r30X1DZQ/file.html',
+    link: 'https://50projects50days.com/projects/quiz-app/',
+  },
+];
